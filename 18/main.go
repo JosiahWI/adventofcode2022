f3a313250e3db1ccf4e50716bf02ae497320c174
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2022-12-19 11:30:56 +0000

    add day 18 part 2 in Go

--- 18/main.go
+++ 18/main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -32,33 +31,76 @@ func main() {
 			fmt.Fprintf(os.Stderr, "day18: %v\n", err)
 		}
 
+		flood := floodfill(vertices)
 		results := make(chan int)
-		for i, _ := range vertices {
-			go countEdges(vertices[i], vertices[i+1:], results)
+		for _, vert := range vertices {
+			go countEdges(vert, flood, results)
 
 		}
 
 		vertexCount := len(vertices)
-		surfaceArea := 6 * vertexCount
+		surfaceArea := 0
 		for i := 0; i < vertexCount; i++ {
-			surfaceArea -= <-results
+			surfaceArea += <-results
 		}
 
 		fmt.Printf("%d\t%s\n", surfaceArea, filename)
 	}
 }
 
-func countEdges(here Vec3d, others []Vec3d, res chan<- int) {
+func floodfill(vertices []Vec3d) map[Vec3d]bool {
+	var maxx, maxy, maxz int
+	cubemap := make(map[Vec3d]bool)
+	for _, vert := range vertices {
+		cubemap[vert] = true
+		if vert.x > maxx {
+			maxx = vert.x
+		}
+		if vert.y > maxy {
+			maxy = vert.y
+		}
+		if vert.z > maxz {
+			maxz = vert.z
+		}
+	}
+
+	maxx, maxy, maxz = maxx+1, maxy+1, maxz+1
+
+	flood := make(map[Vec3d]bool)
+	inBounds := func(vert Vec3d) bool {
+		return -1 <= vert.x && vert.x <= maxx && -1 <= vert.y && vert.y <= maxy && -1 <= vert.z && vert.z <= maxz
+	}
+
+	offsets := []Vec3d{Vec3d{1, 0, 0}, Vec3d{-1, 0, 0}, Vec3d{0, 1, 0}, Vec3d{0, -1, 0}, Vec3d{0, 0, 1}, Vec3d{0, 0, -1}}
+
+	for queue := []Vec3d{Vec3d{-1, -1, -1}}; len(queue) > 0; {
+		v := queue[0]
+		queue = queue[1:]
+		for _, off := range offsets {
+			adj := Vec3d{v.x + off.x, v.y + off.y, v.z + off.z}
+			if !inBounds(adj) || cubemap[adj] || flood[adj] {
+				continue
+			}
+
+			flood[adj] = true
+			queue = append(queue, adj)
+		}
+	}
+
+	return flood
+}
+
+func countEdges(here Vec3d, flood map[Vec3d]bool, res chan<- int) {
+	offsets := []Vec3d{Vec3d{1, 0, 0}, Vec3d{-1, 0, 0}, Vec3d{0, 1, 0}, Vec3d{0, -1, 0}, Vec3d{0, 0, 1}, Vec3d{0, 0, -1}}
+
 	edges := 0
-	for _, there := range others {
-		dx := math.Abs(float64(here.x - there.x))
-		dy := math.Abs(float64(here.y - there.y))
-		dz := math.Abs(float64(here.z - there.z))
-		if dx+dy+dz == 1 {
+	for _, off := range offsets {
+		adj := Vec3d{here.x + off.x, here.y + off.y, here.z + off.z}
+		if flood[adj] {
 			edges++
 		}
 	}
-	res <- 2 * edges
+	res <- edges
 }
 
 func parse(data string) ([]Vec3d, error) {
