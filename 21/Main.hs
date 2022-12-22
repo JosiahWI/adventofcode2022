809bf65315a3844427f6390c15746dc1028205e2
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2022-12-22 01:05:49 +0000

    solve day 21 part 2 in Haskell

--- 21/Main.hs
+++ 21/Main.hs
@@ -16,7 +16,7 @@ data Expr a =
 data BinaryTree a = BinaryTree (BinaryTree a) a (BinaryTree a) | Leaf
   deriving Show
 
-data Monkey = Monkey Name (Expr Int)
+data Monkey = Monkey Name (Expr Rational)
   deriving Show
 
 mkOp :: String -> Operation
@@ -30,22 +30,22 @@ mkOp op
 mkMonkey :: String -> Monkey
 mkMonkey s =
   case words s of
-    (name:val:[])    -> Monkey (init name) (Value (read val))
+    (name:val:[])    -> Monkey (init name) (Value (fromIntegral $ read val))
     (name:a:ops:b:[]) -> Monkey (init name) (Expr a (mkOp ops) b)
 
-ap :: Operation -> Int -> Int -> Int
+ap :: Operation -> Rational -> Rational -> Rational
 ap op a b
   | op == Add = a + b
   | op == Sub = a - b
   | op == Mul = a * b
-  | op == Div = a `div` b
+  | op == Div = a / b
 
 insert :: MonkeyTree -> Monkey -> MonkeyTree
 insert Leaf a = BinaryTree Leaf a Leaf
 insert tree@(BinaryTree left val@(Monkey name _) right) a@(Monkey name' _)
   | name' < name   = BinaryTree (insert left a) val right
   | name' > name   = BinaryTree left val (insert right a)
-  | otherwise      = tree
+  | otherwise      = BinaryTree left a right
 
 find :: MonkeyTree -> Name -> Monkey
 find Leaf name   = error ("could not find monkey " ++ name)
@@ -54,7 +54,7 @@ find (BinaryTree left m@(Monkey name _) right) name'
   | name' > name = find right name'
   | otherwise    = m
 
-eval :: MonkeyTree -> Name -> Int
+eval :: MonkeyTree -> Name -> Rational
 eval tree name = case find tree name of
                    (Monkey _ (Value a))     -> a
                    (Monkey _ (Expr a op b)) -> ap op (eval tree a) (eval tree b)
@@ -64,3 +64,9 @@ main = do
   contents <- readFile "input.txt"
   let tree = foldl insert Leaf $ map mkMonkey $ lines contents
   print $ eval tree "root"
+  let (Monkey _ (Expr a _ b)) = find tree "root"
+  let subbed = insert tree (Monkey "root" (Expr a Sub b))
+  let subbed' = insert subbed (Monkey "humn" (Value 3555057453229))
+  let res = eval subbed' "root"
+  print $ res
+  print $ ((fromRational res) :: Double)
